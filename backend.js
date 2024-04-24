a29574c98619a7e4f46c3f79981e0ecbc12b57e3
Kenneth Tschida <158073504+Kenny-Tschida@users.noreply.github.com>
2024-04-24 12:38:20 -0500

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = "8081";
+const host = "localhost";
+const { MongoClient } = require("mongodb");
+
+// MongoDB
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "reactdata";
+const client = new MongoClient(url);
+const db = client.db(dbName);
+
+app.get("/listProducts", async (req, res) => {
+  await client.connect();
+  console.log("Node connected successfully to GET MongoDB");
+  const query = {};
+  const results = await db
+    .collection("fakestore_catalog")
+    .find(query)
+    .limit(100)
+    .toArray();
+  console.log(results);
+  res.status(200);
+  res.send(results);
+});
+
+app.get("/:id", async (req, res) => {
+  const productid = Number(req.params.id);
+  console.log("Product to find :", productid);
+  await client.connect();
+  console.log("Node connected successfully to GET-id MongoDB");
+  const query = { id: productid };
+  const results = await db.collection("fakestore_catalog").findOne(query);
+  console.log("Results :", results);
+  if (!results) res.send("Not Found").status(404);
+  else res.send(results).status(200);
+});
+
+app.listen(port, () => {
+  console.log("App listening at http://%s:%s", host, port);
+});
